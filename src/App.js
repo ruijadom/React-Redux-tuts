15162d406f6445ab5f97d14b8ba125c19bb95545
ruijadom <ruijadomingues@gmail.com>
2020-01-04 09:50:26 +0000

    add modules

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import "./App.css";
+import CakeContainer from "./components/CakeContainer";
 
 function App() {
   return (
     <div className="App">
       <p>React & Redux</p>
+      <CakeContainer></CakeContainer>
     </div>
   );
 }
